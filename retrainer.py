7e842c8c1b8b492e454eabffe3de009cb897c493
JLanan <justin.lanan@gmail.com>
2023-10-07 04:46:56 -0400

    getting ready for augmentation

---
+++ retrainer.py
@@ -0,0 +1,4 @@
+# use_augmentations = True/False
+# random state is a parameter specified by user
+# Let user select True/False for each augmentation subfunction
+# She uses black padding when correcting tile size of shrunken images
