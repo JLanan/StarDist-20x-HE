c87cfdbddae20e335193223a9fc186387df5e746
JLanan <justin.lanan@gmail.com>
2023-10-28 16:06:50 -0400

    stuff

--- examples/evaluator.py
+++ examples/evaluator.py
@@ -15,5 +15,4 @@ scorer = tp.TileSetScorer(base_names=tile_sets[0], gt_set=tile_sets[1][0], pred_
 results_granular = scorer.df_results_granular
 results_summary = scorer.df_results_summary
 
-x=5
 # Save dataframes to .csv or excel as needed
--- making_base_model/b1_parameter_tune_coarse.py
+++ making_base_model/b1_parameter_tune_coarse.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pandas as pd
 from my_utils import tile_processing as tp
 from my_utils import stardisting as sd
 # graph average mAP and average IoU (with stdev error bars) and see if high points agree
@@ -33,26 +34,51 @@ def read_single_dataset_train_and_validate_data(root_dir: str, ext: str, dataset
     return trn, vld
 
 
+def run_experiment(trn: list, vld: list, tst: list, folder: str, epks: range, lrs: list, bss: list, fold: str, trn_set: str) \
+        -> (pd.DataFrame, pd.DataFrame):
+    id_cols = ['Train Set', 'Train Fold', 'LR', 'BS', 'Epoch', 'Test Set']
+    df_ids = pd.DataFrame(columns=id_cols)
+    for lr in lrs:
+        for bs in bss:
+            for epk in epks:
+                for dataset in tst:
+                    for i in range(len(dataset[1])):
+                        # One line id dataframe
+                        df_id_1 = {'Train Set': [trn_set],
+                                   'Train Fold': [fold],
+                                   'LR': [lr],
+                                   'BS': [bs],
+                                   'Epoch': [epk],
+                                   'Test Set': [dataset]}
+                        df_id_1 = pd.DataFrame(data=df_id_1)
+                        
+
+
+
+
+
+
+            model = sd.load_published_he_model(folder, 'hyperparameter_tuning')
+            model = sd.configure_model_for_training(model=model, epochs=5, learning_rate=lr, batch_size=bs)
+
+
+                df_gran_right, df_sum_right = sd.predict_and_score_on_test_data(model, test)
+                model = sd.normalize_train_and_threshold(model, train[1], train[2], validate[1], validate[2])
+    return df_gran, df_sum
+
+
 if __name__ == "__main__":
-    root_directory = r"\\babyserverdw5\Digital pathology image lib\_Image libraries for training\2023-05-09 Published HE Nuclei Datasets\20x_split"
+    directory_20x_split = r"\\babyserverdw5\Digital pathology image lib\_Image libraries for training\2023-05-09 Published HE Nuclei Datasets\20x_split"
     extension = '.tif'
-    test = read_test_data(root_directory, extension)
+    test = read_test_data(directory_20x_split, extension)
 
     dataset_name = 'MoNuSeg'
     fold_name = 'fold_1'
-    train, validate = read_single_dataset_train_and_validate_data(root_directory, extension, dataset_name, fold_name)
+    train, validate = read_single_dataset_train_and_validate_data(directory_20x_split, extension, dataset_name, fold_name)
 
     landing_folder_for_models = r"\\babyserverdw5\Digital pathology image lib\_Image libraries for training\2023-05-09 Published HE Nuclei Datasets\StarDist Training\models"
     epochs = range(0, 105, 5)
     learning_rates = [1e-7, 1e-6, 1e-5]
     batch_sizes = [4, 8, 16]
 
-    id_columns = ['Train Set', 'Train Fold', 'LR', 'BS', 'Epoch', 'Test Set']  # tile name is part of score columns
-    for lr in learning_rates:
-        for bs in batch_sizes:
-            model = sd.load_published_he_model(landing_folder_for_models, 'hyperparameter_tuning')
-            model = sd.configure_model_for_training(model=model, epochs=5, learning_rate=lr, batch_size=bs)
-            for epk in epochs:
-                df_results = sd.predict_and_score_on_test_data(model, test)
-                model = sd.normalize_train_and_threshold(model, train[1], train[2], validate[1], validate[2])
-
+    df_granular, df_summary = run_experiment(landing_folder_for_models, epochs, learning_rates, batch_sizes, fold_name, dataset_name)
