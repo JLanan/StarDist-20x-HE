9863267e75684176be37d751b004e0221fdc166d
JLanan <justin.lanan@gmail.com>
2024-05-03 01:37:16 -0400

    stuff

--- wsi_performance/wsi_segmentation.py
+++ wsi_performance/wsi_segmentation.py
@@ -8,18 +8,18 @@ wsi_paths = [r"\\10.99.68.53\Digital pathology image lib\JHU\Ie-Ming Shih\lympho
              r"\\10.99.68.53\Digital pathology image lib\JHU\Laura Wood\Raw image Pancreas SenPanc001\raw images\OTS-22-15061-001 - 2022-12-19 21.46.09.ndpi",
              r"\\10.99.68.53\Digital pathology image lib\HubMap Skin TMC project\HM-SR1-Skin-P010-B1-SB01\raw_images\he.ndpi"]
 model_names = ['2D_versatile_he', 'SD_HE_20x']
-output_folders = [r"Z:\WSI Results\MODELNAME\Fallopian_Tube",
-                  r"Z:\WSI Results\MODELNAME\Pancreas",
+output_folders = [r"\\10.99.68.53\Digital pathology image lib\_Image libraries for training\2023-05-09 Published HE Nuclei Datasets\WSI Results\MODELNAME\Fallopian_Tube",
+                  r"\\10.99.68.53\Digital pathology image lib\_Image libraries for training\2023-05-09 Published HE Nuclei Datasets\WSI Results\MODELNAME\Pancreas",
                   r"\\10.99.68.53\Digital pathology image lib\_Image libraries for training\2023-05-09 Published HE Nuclei Datasets\WSI Results\MODELNAME\Skin"]
 model_paths = r"\\10.99.68.53\Digital pathology image lib\_Image libraries for training\2023-05-09 Published HE Nuclei Datasets\StarDist Segmentation Models\MODELNAME"
 
 columns = ['Model', 'Tissue', 'Runtime (min)', 'Objects Detected']
 df = pd.DataFrame(columns=columns)
 
-for i, wsi_path in enumerate([wsi_paths[1]]):
+count = 0
+for i, wsi_path in enumerate(wsi_paths):
     for model_name in model_names:
-        # output_folder = output_folders[i].replace("MODELNAME", model_name)
-        output_folder = output_folders[1].replace("MODELNAME", model_name)
+        output_folder = output_folders[i].replace("MODELNAME", model_name)
         model_path = model_paths.replace("MODELNAME", model_name)
 
         start_time = time.time()
@@ -43,9 +43,10 @@ for i, wsi_path in enumerate([wsi_paths[1]]):
         end_time = time.time()
         runtime = round(((end_time - start_time) / 60), 2)
         z_results = segmentation.zarrs  # 0-Mask, 1-Centroids, 2-Vertices
-        wsi_mask = np.array(z_results[0])
-        df.loc[0, 'Model'] = model_path
-        df.loc[0, 'Tissue'] = wsi_path
-        df.loc[0, 'Runtime (min)'] = runtime
-        df.loc[0, 'Objects Detected'] = len(z_results[1])
-        df.to_csv(fr"{output_folder}\metrics.csv", index=False)
+        df.loc[count, 'Model'] = model_path
+        df.loc[count, 'Tissue'] = wsi_path
+        df.loc[count, 'Runtime (min)'] = runtime
+        df.loc[count, 'Objects Detected'] = len(z_results[1])
+        [z.close() for z in z_results]
+        df.to_csv(r"\\10.99.68.53\Digital pathology image lib\_Image libraries for training\2023-05-09 Published HE Nuclei Datasets\WSI Results\metrics.csv", index=False)
+        count += 1
